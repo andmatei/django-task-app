35ba82259d0f1362200a9dcc91bb4f695cfd6733
Andrei Matei <a.matei2206@gmail.com>
2024-08-20 03:25:02 +0100

    fix: remove cached files

--- static/CACHE/js/output.279daae1f098.js
+++
@@ -1,1 +0,0 @@
-{"counts":{"urget":1,"low_priority":0,"medium_priority":0,"high_priority":1}};
--- static/CACHE/js/output.c1cf271bfb75.js
+++
@@ -1,1 +0,0 @@
-<script id="stats"type="application/json">{"counts":{"urget":1,"low_priority":0,"medium_priority":0,"high_priority":1}};
