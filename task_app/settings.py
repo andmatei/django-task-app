ef28e2c7297b3ee49021779c2310533dbf76e3e3
Kristaps Eisaks <kristapseisaks03@gmail.com>
2024-10-09 20:48:27 +0100

    Completion
    - added settings for database
    - added new dependency psycopg2

--- task_app/settings.py
+++ task_app/settings.py
@@ -76,13 +76,25 @@ WSGI_APPLICATION = 'task_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'task_app',
+        'USER': 'task_app',
+        'PASSWORD': 'justapassword',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
